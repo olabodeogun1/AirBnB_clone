6557e115bacb413fc2f4563ac32a094454452683
Olabode Ogun <olabodeogun1@gmail.com>
2023-05-09 14:58:27 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Base model for all objects """
 
+import uuid
+from datetime import datetime
+
 
 class BaseModel:
     """
@@ -8,3 +11,26 @@ class BaseModel:
     def __init__(self):
         """
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        """
+        return f'[<{self.__class__.__name__}>] (<{self.id}>) <{self.__dict__}>'
+
+    def save(self):
+        """
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+
---
+++ test_base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("START" * 5)
+
+test = BaseModel()
+test.name = "My Second Model"
+test.my_number = 76
+print(test)
+test.save()
+print(test)
+test_json = test.to_dict()
+print(test_json)
+print("JSON OF MY_TEST: ")
+for key in test_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(test_json[key]), test_json[key]))
