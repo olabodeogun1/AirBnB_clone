a493e31d879b1f858552b332d8f94452b6795079
Noah <noahayebu@gmail.com>
2023-05-11 07:29:31 +0000

    edits to private str method

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
     def __str__(self):
         """
         """
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return (f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
 
     def save(self):
         """
